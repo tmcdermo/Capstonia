d4c9d2203f6f78748ddb00cfca5cafca81b7f453
Tommy <mcdermot@oregonstate.edu>
2018-10-21 12:44:02 -0400

    Basic Project/Structure Setup
    Add LFS .png tracking

--- Controller/Program.cs
+++ Controller/Program.cs
@@ -1,6 +1,6 @@
 ﻿using System;
 
-namespace Capstonia
+namespace Capstonia.Controller
 {
     /// <summary>
     /// The main class.
@@ -13,7 +13,7 @@ namespace Capstonia
         [STAThread]
         static void Main()
         {
-            using (var game = new Game1())
+            using (var game = new GameManager())
                 game.Run();
         }
     }
